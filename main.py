2c31bf902c544d26b1742f456f55be90ca2afa97
Kurma ritish <136564903+Ritish017@users.noreply.github.com>
2025-10-15 14:54:45 +0530

    Add main.py - ReAct Agent implementation

---
+++ main.py
@@ -0,0 +1,82 @@
+from typing import Union
+from dotenv import load_dotenv
+from langchain.agents import tool, AgentAction, AgentFinish
+from langchain.prompts import PromptTemplate
+from langchain.tools import render_text_description
+from langchain_groq import ChatGroq
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain.agents.format_scratchpad import format_log_to_str
+
+load_dotenv() 
+
+
+
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the Length of given text."""
+    print(f"get_text-length enter with {text=}")
+    text = text.strip("'\n").strip('"')
+    return len(text)
+
+def find_tool_by_name(tools:list, tool_name:str):
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found")   
+
+
+if __name__ == "__main__":
+    print("Hello from ReAct Agent!")
+    tools = [get_text_length]
+    
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought: {agent_scratchpad}
+    """
+    prompt = PromptTemplate.from_template(
+        template=template
+    ).partial(tools=render_text_description(tools), tool_names= ", ".join([t.name for t in tools]))
+    
+    llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768",stop = ["\nObservation:"])
+    
+    intermediate_steps = []
+    
+    
+
+    agent ={"input":lambda x:x["input"], "agent_scratchpad":lambda x:format_log_to_str(x["agent_scratchpad"])} | prompt| llm | ReActSingleInputOutputParser()
+
+    agent_step: Union[AgentAction, AgentFinish]= agent.invoke({"input": "What is the text length of 'DOG' in characters?","agent_scratchpad":intermediate_steps})
+    print(agent_step)
+    
+    if isinstance(agent_step,AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+        
+        observation = tool_to_use(str(tool_input))
+
+        print(f"{observation=}")
+        intermediate_steps.append((agent_step, str(observation)))
+    agent_step: Union[AgentAction, AgentFinish]= agent.invoke({"input": "What is the text length of 'DOG' in characters?","agent_scratchpad": intermediate_steps})
+    print(agent_step)
+    
+    if isinstance(agent_step,AgentFinish):
+        print(f"Agent finished with {agent_step.return_values=}")
+        
